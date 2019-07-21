3412ab3a3e0ae3a2259f3bf63b09a518a37470d9
thethir13en <thethir13en@gmail.com>
2019-07-21 11:08:17 +0300

    cleanup from previous homework

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "app_external_ips" {
   value = "${google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip}"
 }
 
-output "lb_external_ip" {
-  value = "${google_compute_forwarding_rule.default.ip_address}"
-}
+# output "lb_external_ip" {
+#   value = "${google_compute_forwarding_rule.default.ip_address}"
+# }
