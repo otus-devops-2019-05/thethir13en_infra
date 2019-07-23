09366fcd0b784aabd7824604da879cfd2f9c6e01
thethir13en <thethir13en@gmail.com>
2019-07-23 23:55:03 +0300

    provide private_key_path and db_url to app instance

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -21,8 +21,10 @@ module "vpc" {
 module "app" {
   source = "../modules/app"
   public_key_path = "${var.public_key_path}"
+  private_key_path = "${var.private_key_path}"
   zone = "${var.zone}"
   app_disk_image = "${var.app_disk_image}"
+  db_url= "${module.db.internal_ip}"
 }
 module "db" {
   source = "../modules/db"
