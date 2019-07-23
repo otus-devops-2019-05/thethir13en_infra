a990816fef044c9cc7a6ccb6b39f4b969416adbc
thethir13en <thethir13en@gmail.com>
2019-07-23 23:53:43 +0300

    update app-module variables with db_url and private_key_path

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -7,7 +7,16 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
+variable private_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
 variable app_disk_image {
   description = "Disk image for reddit app"
   default = "reddit-app-base"
+}
+
+variable "db_url" {
+  description = "Hostname or IP of MongoDB"
+  default = ""
 }
